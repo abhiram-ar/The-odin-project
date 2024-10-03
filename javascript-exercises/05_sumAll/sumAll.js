97352e555664c16cf82fd996d892cb5680485fd1
abhiram-ar <abhiram2001.ars@gmail.com>
2024-10-03 14:00:24 +0530

    sum al

--- javascript-exercises/05_sumAll/sumAll.js
+++ javascript-exercises/05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(...num) {
+    num.sort()
+    for (let n of num){
+        if(n < 0 || typeof(n) !== "number" || n%1 !== 0){
+            return "ERROR"
+        }
+    }
+    
+   
+    let res = 0;
+    for(let i = num[0]; i<=num[1]; i++){
+        res += i
+    }
+    return res
 };
 
 // Do not edit below this line
