ff00da27223f7c14f20b62abefff928a8c3a9b98
abhiram-ar <abhiram2001.ars@gmail.com>
2024-10-03 12:59:45 +0530

    passed repeat sting

--- javascript-exercises/02_repeatString/repeatString.js
+++ javascript-exercises/02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(str, repeat) {
+    if(repeat < 0) return "ERROR"
+    if(str === "") return ""
 
+    let res = ""
+    for(let i=0; i<repeat; i++){
+        res += str
+    }
+    return res
 };
 
 // Do not edit below this line
